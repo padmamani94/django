a835c15d98bd73bf8e4d4568dfbb1b5957c0c871
padmamani94 <padmavathimani30@gmail.com>
2026-05-22 01:44:19 +0530

    Your file uploaded

---
+++ django-ecommerce/store/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, id=pk)
+    return render(request, 'store/detail.html', {'product': product})
